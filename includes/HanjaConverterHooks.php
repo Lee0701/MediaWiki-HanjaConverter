54488a884b129a6d76f0a6dc6172a5b6eaf5828c
Lee0701 <hgl1002@naver.com>
2020-09-06 15:32:27 +0900

    Add user preference for ruby

--- includes/HanjaConverterHooks.php
+++ includes/HanjaConverterHooks.php
@@ -12,7 +12,8 @@ class HanjaConverterHooks {
     public static function onHtmlPageLinkRendererBegin(LinkRenderer $linkRenderer, LinkTarget $target, &$text, &$extraAttribs, &$query, &$ret) {
         if(!($target instanceof Title)) return true;
         if(!($text instanceof HtmlArmor)) return true;
-        // if($target->isKnown()) return true;
+        $unknown = '';
+        if(!$target->isKnown()) $unknown = ' unknown';
 
         $userDictionary = UserDictionary::get();
 
@@ -32,7 +33,7 @@ class HanjaConverterHooks {
                         array_push($grades, HanjaGrades::gradeOf(iconv_substr($key, $k, 1)));
                     $grade = min($grades);
 
-                    $result .= Ruby::format($key, $value, "grade$grade");
+                    $result .= Ruby::format($key, $value, "grade$grade$unknown");
                     $i += iconv_strlen($value);
                     $found = true;
                     break;
@@ -47,7 +48,28 @@ class HanjaConverterHooks {
     }
     
     public static function onBeforePageDisplay(OutputPage $outputPage, Skin $skin) {
-        $outputPage->addModuleStyles('ext.HanjaConverter.ruby');
+        $outputPage->addModuleStyles('ext.HanjaConverter.ruby.hide');
+        $outputPage->addModules('ext.HanjaConverter.ruby.show');
+    }
+
+    public static function onGetPreferences(User $user, array &$preferences) {
+        $preferences['displayRubyForUnknownLink'] = [
+            'type' => 'toggle',
+            'label-message' => 'tog-HanjaConverter-displayRubyForUnknownLink',
+            'section' => 'rendering',
+        ];
+        $grades = array(0, 10, 20, 30, 32, 40, 42, 50, 52, 60, 62, 70, 72, 80);
+        $options = array();
+        foreach($grades as $grade) {
+            $options[wfMessage("tog-HanjaConverter-grade$grade")->parse()] = "grade$grade";
+        }
+        $preferences['displayRubyForGrade'] = [
+            'type' => 'radio',
+            'label-message' => 'tog-HanjaConverter-displayRubyForGrade',
+            'options' => $options,
+            'default' => $user->getOption('displayRubyForGrade', 'grade0'),
+            'section' => 'rendering',
+        ];
     }
 }
 
--- includes/Ruby.php
+++ includes/Ruby.php
@@ -3,8 +3,8 @@
 class Ruby {
     public static function format($hanja, $reading, $class) {
         $classes = "";
-        if($class) $classes = " class=\"hanja $class\"";
-        return "<ruby$classes><rb>$hanja</rb><rt>$reading</rt><rp>($reading)</rp></ruby$classes>";
+        if($class) $classes = "class=\"hanja $class\"";
+        return "<ruby $classes><rb>$hanja</rb><rt>$reading</rt><rp>($reading)</rp></ruby>";
     }
 }
 
