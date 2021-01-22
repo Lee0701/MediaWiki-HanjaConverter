13ba72dd1ab943329bfdafacff9170c5cae6b082
Lee0701 <hgl1002@naver.com>
2021-01-22 18:44:42 +0900

    update dictionary

--- convert_dictionary.js
+++ convert_dictionary.js
@@ -7,17 +7,22 @@ const lines = contents.map((content) => content.split('\n')).flat()
 const commentLines = lines.filter((line => line.startsWith('#')))
 const dataLines = lines.filter((line) => !line.startsWith('#') && line.trim() !== '')
 
-const dict = dataLines.map((line) => line.split('\t'))
+const dict = dataLines.map((line) => line.split('\t')).filter((entry) => entry.length >= 2)
         .reduce((a, [hanja, reading]) => (a[hanja] = a[hanja] ? a[hanja] : reading.replace(/\s/g, ''), a), {})
 
-const grade = (hanja) => hanja.split('').filter((c) => !(c >= '가' && c <= '힣' || c >= 'ㄱ' && c <= 'ㅣ')).map((c) => {
+const grade = (hanja) => hanja.split('').filter((c) => c.replace(/[가-힣ㄱ-ㅎㅏ-ㅣ]/g, '')).map((c) => {
     const grade = Object.entries(hanjaGrades).find(([_grade, list]) => list.indexOf(c) != -1)
     if(grade === undefined) return 0
     else return parseInt(grade[0])
-}).reduce((a, c) => a < c ? a : c)
+})
+
+const minGrade = (hanja) => grade(hanja).reduce((a, c) => a < c ? a : c)
+const maxGrade = (hanja) => grade(hanja).reduce((a, c) => a > c ? a : c)
 
 const result = Object.entries(dict)
-        .map(([hanja, reading]) => `"${hanja}" => "${reading}:${grade(hanja)}"`)
+        .sort(([_hanja, reading], [_hanja2, reading2]) => reading.localeCompare(reading2))
+        .filter(([hanja, reading]) => !(maxGrade(hanja) == 0 && hanja.length > 1 || hanja.length > 5 || hanja.match(/[가-힣ㄱ-ㅎㅏ-ㅣ]/)))
+        .map(([hanja, reading, grade]) => `"${hanja}"=>"${reading}:${minGrade(hanja)}"`)
         .join(',\n')
 const comment = commentLines.join('\n')
 console.log([
