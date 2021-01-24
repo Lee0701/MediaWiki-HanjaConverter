e465e27dbde822b8bde71c424fa9afe7c337565c
Lee0701 <hgl1002@naver.com>
2021-01-25 03:14:47 +0900

    Optimise dictionary by removing duplicated words

--- gen_dictionary.js
+++ gen_dictionary.js
@@ -1,11 +1,12 @@
 
 const fs = require('fs')
 
-const chunkSize = 10
-
 const dic2 = fs.readFileSync('dicts/dic2.txt').toString().split('\n')
 const hanjaTxt = fs.readFileSync('dicts/hanja.txt').toString().split('\n')
 const krStdict = fs.readFileSync('dicts/kr-stdict.tsv').toString().split('\n')
+
+const chunkSize = 10
+const chunk = (arr) => new Array(Math.ceil(arr.length / chunkSize)).fill().map((_, i) => arr.slice(i*chunkSize, (i+1)*chunkSize))
 
 const commentLines = [dic2, hanjaTxt, krStdict].flat()
         .filter((line => line.startsWith('#')))
@@ -29,9 +30,30 @@ Object.entries(dict).forEach(([key, value]) => {
 
 const table = Object.entries(dict)
         .sort(([_hanja, reading], [_hanja2, reading2]) => reading.localeCompare(reading2))
-        .filter(([hanja, reading]) => hanja.length == reading.length && !(hanja.length > 5 || hanja.match(/[가-힣ㄱ-ㅎㅏ-ㅣ]/)))
-        .map(([hanja, reading]) => `"${hanja}"=>"${reading}"`)
-const chunked = new Array(Math.ceil(table.length / chunkSize)).fill().map((_, i) => table.slice(i*chunkSize, (i+1)*chunkSize))
+        .filter(([hanja, reading]) => hanja.length == reading.length && !(hanja.match(/[가-힣ㄱ-ㅎㅏ-ㅣ]/)))
+        .reduce((a, [hanja, reading]) => (a[hanja] = reading, a), {})
+
+const filtered = Object.entries(table).filter(([hanja, reading]) => {
+    if(hanja.length == 1) return true
+    for(let i = 0; i < hanja.length;) {
+        let found = false
+        for(let j = hanja.length - i; j > 0; j--) {
+            const key = hanja.slice(i, i + j)
+            if(key == hanja) continue
+            const value = table[key]
+            // console.log(key, value, reading.slice(i, i + j))
+            if(value == reading.slice(i, i + j)) {
+                i += j
+                found = true
+                break
+            }
+        }
+        if(!found) return true
+    }
+    return false
+})
+
+const chunked = chunk(filtered.map(([hanja, reading]) => `"${hanja}"=>"${reading}"`))
 const result = chunked.map((chunk) => chunk.join(',')).join(',\n')
 const comment = commentLines.join('\n')
 console.log([
