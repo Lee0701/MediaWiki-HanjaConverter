c68247d70c1f83ad06ca43c512fd025573b68393
Lee0701 <hgl1002@naver.com>
2021-01-29 23:04:43 +0900

    Fix dictionary generation

--- gen_dictionary.js
+++ gen_dictionary.js
@@ -3,15 +3,22 @@ const fs = require('fs')
 
 const dic2 = fs.readFileSync('dicts/dic2.txt').toString().split('\n')
 const hanjaTxt = fs.readFileSync('dicts/hanja.txt').toString().split('\n')
-const krStdict = fs.readFileSync('dicts/kr-stdict.tsv').toString().split('\n')
 
 const chunkSize = 10
 const chunk = (arr) => new Array(Math.ceil(arr.length / chunkSize)).fill().map((_, i) => arr.slice(i*chunkSize, (i+1)*chunkSize))
 
-const commentLines = [dic2, hanjaTxt, krStdict].flat()
+const initialSoundLaw = (c) => {
+    const normalized = c.normalize('NFD').split('')
+    if(normalized[0] == 'ᄅ') normalized[0] = 'ᄂ'
+    if(normalized[0] == 'ᄂ' && 'ᅣᅤᅧᅨᅭᅲᅵ'.includes(normalized[1])) normalized[0] = 'ᄋ'
+    return normalized.join('').normalize('NFC')
+}
+
+
+const commentLines = [['# (File: dic2.txt)', '#'], dic2, ['#', '# (File: hanja.txt)', '#'], hanjaTxt].flat()
         .filter((line => line.startsWith('#')))
 
-const dict = [dic2, krStdict].flat()
+const dic2Dict = dic2
         .filter((line) => !line.startsWith('#') && line.trim() !== '')
         .map((line) => line.split('\t')).filter((entry) => entry.length >= 2)
         .reduce((a, [hanja, reading]) => (a[hanja] = a[hanja] ? a[hanja] : reading.replace(/\s/g, ''), a), {})
@@ -24,24 +31,20 @@ hanjaTxt
             hanjaTxtDict[hanja].push(reading)
         })
 
-Object.entries(dict).forEach(([key, value]) => {
-    if(hanjaTxtDict[key] && !hanjaTxtDict[key].includes(value)) dict[key] = hanjaTxtDict[key][0]
-})
-
-const initialSoundLaw = (c) => {
-    const normalized = c.normalize('NFD').split('')
-    if(normalized[0] == 'ᄅ') normalized[0] = 'ᄂ'
-    if(normalized[0] == 'ᄂ' && 'ᅣᅤᅧᅨᅭᅲᅵ'.includes(normalized[1])) normalized[0] = 'ᄋ'
-    return normalized.join('').normalize('NFC')
-}
-
-const table = Object.entries(dict)
+const table = Object.entries(hanjaTxtDict)
+        .map(([hanja, readings]) => readings.map((reading) => [hanja, reading])).flat()
+        .filter(([hanja, reading]) => hanja.length > 1 || dic2Dict[hanja] == reading)
         .sort(([_hanja, reading], [_hanja2, reading2]) => reading.localeCompare(reading2))
-        .filter(([hanja, reading]) => hanja.length == reading.length && !(hanja.match(/[가-힣ㄱ-ㅎㅏ-ㅣ]/)))
         .reduce((a, [hanja, reading]) => (a[hanja] = reading, a), {})
 
 const filtered = Object.entries(table).filter(([hanja, reading]) => {
     if(hanja.length == 1) return true
+    if(hanja.length != reading.length) return false
+    for(let i = 0; i < hanja.length; i++) {
+        const h = hanja.charAt(i)
+        const r = reading.charAt(i)
+        if(!hanjaTxtDict[h] || !hanjaTxtDict[h].includes(r)) return false
+    }
     let initial = true
     for(let i = 0; i < hanja.length;) {
         let found = false
